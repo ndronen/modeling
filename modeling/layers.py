4e944a01a30936672b1199e2830951a3f275d95f
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-12-25 12:04:48 -0700

    Add modeling.layers.ImmutableConvolution1D

--- modeling/layers.py
+++ modeling/layers.py
@@ -1,29 +1,46 @@
 import logging
 import theano.tensor as T
 from keras.layers.embeddings import Embedding
+from keras.layers.convolutional import Convolution1D
 from keras.layers.core import Layer
 
 logger = logging.getLogger()
 
 class ImmutableEmbedding(Embedding):
     '''
-        Same as keras.layers.Embedding except the weights are parameters
-        of the network.  This can be useful when the layer is initialized
-        with pre-trained embeddings, such as Word2Vec.
+    Same as Embedding except the weights are not parameters of the
+    network.  This can be useful when the layer is initialized with
+    pre-trained embeddings, such as Word2Vec.
 
-        @input_dim: size of vocabulary (highest input integer + 1)
-        @out_dim: size of dense representation
+    @input_dim: size of vocabulary (highest input integer + 1)
+    @out_dim: size of dense representation
     '''
     def __init__(self, input_dim, output_dim, **kwargs):
-
         super(ImmutableEmbedding, self).__init__(
                 input_dim, output_dim, **kwargs)
-
-        print("W", self.get_weights())
-        print("params", self.params)
         self.params = []
-        print("params", self.params)
 
+    def build(self):
+        super(ImmutableEmbedding, self).build()
+        self.params = []
+
+class ImmutableConvolution1D(Convolution1D):
+    '''
+    Same as Convolution1D except the convolutional filters are not 
+    parameters of the network.  This can be useful when the layer 
+    is initialized with pre-trained convolutional filters.
+
+    @nb_filters: the number of convolutional filters
+    @filter_width: the width of each filter
+    '''
+    def __init__(self, nb_filters, filter_width, **kwargs):
+        super(ImmutableConvolution1D, self).__init__(
+                nb_filters, filter_width, **kwargs)
+        self.params = []
+
+    def build(self):
+        super(ImmutableConvolution1D, self).build()
+        self.params = []
 
 class Transpose(Layer):
     def __init__(self):
