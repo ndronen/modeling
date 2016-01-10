df25340ca34ece80e039dda7f766580a08b7be9d
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-01-09 23:18:50 -0700

    Got attention model working.

--- models/keras/attention/model.py
+++ models/keras/attention/model.py
@@ -6,7 +6,9 @@ import h5py
 import numpy as np
 
 from keras.models import Sequential, Graph
-from keras.layers.core import Dense, Dropout, Activation, Flatten, Layer
+from keras.layers.core import (Layer, Dense, Activation, Dropout,
+        TimeDistributedDense, TimeDistributedMerge,
+        Flatten, Reshape)
 from keras.layers.normalization import BatchNormalization
 from keras.layers.recurrent import LSTM, GRU
 from keras.layers.embeddings import Embedding
@@ -34,7 +36,7 @@ class Identity(Layer):
         return self.get_input(train)
 
 class Transpose(Layer):
-    def get_output(self, train)
+    def get_output(self, train):
         return self.get_input(train).T
 
 def build_model(args):
@@ -52,22 +54,29 @@ def build_model(args):
         return_sequences=True),
         input='embedding', name='lstm0')
 
-    # Build a sequential MLP to add as an attention module.
-    attention = Sequential()
-    attention.add(Dense(100, activation='relu', input_shape=(args.n_units,))
-    attention.add(Dense(100, activation='relu'))
-    attention.add(Activation('softmax'))
+    graph.add_node(LSTM(args.n_units,
+        truncate_gradient=args.truncate_gradient,
+        return_sequences=True),
+        input='lstm0', name='lstm1')
 
-    graph.add_node(Transpose(), input='lstm0', name='transpose0')
-    graph.add_node(attention(), input='transpose', name='attention')
-    graph.add_node(Transpose(), input='attention', name='transpose1')
+    # Attention module.
+    graph.add_node(TimeDistributedDense(args.n_units, activation='relu'),
+            input='lstm1', name='attention0')
+    graph.add_node(TimeDistributedDense(args.n_units, activation='relu'),
+            input='attention0', name='attention1')
+    graph.add_node(TimeDistributedDense(args.n_units, activation='softmax'),
+            input='attention1', name='attention2')
 
-    graph.add_node(inputs=['lstm0', 'transpose1'], merge_mode='mul')
+    # Apply mask from output of attention module to LSTM output.
+    graph.add_node(TimeDistributedMerge(mode='sum'),
+            inputs=['lstm1', 'attention2'],
+            name='applyattn',
+            merge_mode='mul')
 
-    graph.add_node(build_dense_layer(args, args.n_classes,
-            activation='softmax'), name='softmax', input=prev_layer)
+    graph.add_node(Dense(args.n_classes, activation='softmax'),
+            input='applyattn', name='softmax')
 
-    graph.add_output(name='output', input='softmax')
+    graph.add_output(input='softmax', name='output')
 
     load_weights(args, graph)
 
