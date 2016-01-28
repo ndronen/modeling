fb41f84768a278baf7a41ce14eea6496bd3c76b0
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-01-28 11:08:32 -0700

    Update spelling error detection model.

--- models/keras/spelling/convnet/model.py
+++ models/keras/spelling/convnet/model.py
@@ -1,86 +1,54 @@
+import sys
+#sys.setrecursionlimit(5000)
+import json
+import h5py
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution1D, MaxPooling1D
+from keras.layers.core import Dropout, Activation, Flatten
 from keras.layers.normalization import BatchNormalization
-from keras.layers.embeddings import Embedding
-from keras.constraints import maxnorm
-from keras.regularizers import l2
-from keras.optimizers import SGD, Adam, Adadelta, Adagrad, RMSprop
 
-from modeling.layers import ImmutableEmbedding
-from modeling.difference import TemporalDifference
+from modeling.builders import (build_embedding_layer,
+    build_convolutional_layer, build_pooling_layer,
+    build_dense_layer, build_optimizer, load_weights)
 
 def build_model(args):
-    print("args", vars(args))
-
     np.random.seed(args.seed)
 
     model = Sequential()
-
-    if hasattr(args, 'weights') and args.weights is not None:
-        W = np.load(args.weights)
-        model.add(ImmutableEmbedding(args.n_vocab, args.n_word_dims,
-            weights=[W]))
-    else:
-        model.add(Embedding(args.n_vocab, args.n_word_dims,
-            W_constraint=maxnorm(args.embedding_max_norm)))
-
-    if args.use_difference:
-        model.add(TemporalDifference())
-
-    model.add(Convolution1D(args.n_word_dims, args.n_filters, args.filter_width,
-        W_constraint=maxnorm(args.filter_max_norm),
-        border_mode='valid',
-        W_regularizer=l2(args.l2_penalty)))
+    model.add(build_embedding_layer(args))
+    if args.dropout_embedding_p > 0.:
+        model.add(Dropout(args.dropout_embedding_p))
+    model.add(build_convolutional_layer(args))
+    if 'normalization' in args.regularization_layer:
+        model.add(BatchNormalization())
     model.add(Activation('relu'))
+    if args.dropout_conv_p > 0.:
+        model.add(Dropout(args.dropout_conv_p))
 
-    model.add(MaxPooling1D(
-        pool_length=args.input_width-args.filter_width,
-        stride=None, ignore_border=True))
-    model.add(Dropout(0.5))
+    model.add(build_pooling_layer(args))
     model.add(Flatten())
-    if 'dropout' in args.regularization_layer:
-        model.add(Dropout(args.dropout_p_conv))
-    if 'normalization' in args.regularization_layer:
-        model.add(BatchNormalization((args.n_filters,)))
 
-    model.add(Dense(args.n_filters, 2*args.n_filters,
-        W_regularizer=l2(args.l2_penalty)))
-    model.add(Activation('relu'))
-    if 'dropout' in args.regularization_layer:
-        model.add(Dropout(args.dropout_p))
-    if 'normalization' in args.regularization_layer:
-        model.add(BatchNormalization((2*args.n_filters,)))
+    for i in range(args.n_fully_connected):
+        model.add(build_dense_layer(args))
+        if 'normalization' in args.regularization_layer:
+            model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        if 'dropout' in args.regularization_layer:
+            model.add(Dropout(args.dropout_p))
 
-    model.add(Dense(2*args.n_filters, args.n_filters,
-        W_regularizer=l2(args.l2_penalty)))
-    model.add(Activation('relu'))
-    if 'dropout' in args.regularization_layer:
-        model.add(Dropout(args.dropout_p))
-    if 'normalization' in args.regularization_layer:
-        model.add(BatchNormalization((args.n_filters,)))
+    model.add(build_dense_layer(args, args.n_classes,
+            activation='softmax'))
 
-    model.add(Dense(args.n_filters, args.n_classes,
-        W_regularizer=l2(args.l2_penalty)))
-    model.add(Activation('softmax'))
+    load_weights(args, model)
 
-    if args.optimizer == 'SGD':
-        optimizer = SGD(lr=args.learning_rate,
-                decay=args.decay, momentum=args.momentum,
-                clipnorm=args.clipnorm)
-    elif args.optimizer == 'Adam':
-        optimizer = Adam(clipnorm=args.clipnorm)
-    elif args.optimizer == 'RMSprop':
-        optimizer = RMSprop(clipnorm=args.clipnorm)
-    elif args.optimizer == 'Adadelta':
-        optimizer = Adadelta(clipnorm=args.clipnorm)
-    elif args.optimizer == 'Adagrad':
-        optimizer = Adagrad(clipnorm=args.clipnorm)
-    else:
-        raise ValueError("don't know how to use optimizer {0}".format(args.optimizer))
+    optimizer = build_optimizer(args)
 
     model.compile(loss=args.loss, optimizer=optimizer)
+
+    for k,v in json.loads(model.to_json()).items():
+        if k == 'layers':
+            for l in v:
+                print('  %s => %s' %(l['name'], l))
 
     return model
