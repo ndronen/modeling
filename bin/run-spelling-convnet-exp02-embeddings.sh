be7f412da631924ab2f0831f120d0f48e8ebc396
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-02-11 12:37:31 -0700

    Snapshot of script for running embeddings experiment for spelling error detection chater

--- bin/run-spelling-convnet-exp02-embeddings.sh
+++ bin/run-spelling-convnet-exp02-embeddings.sh
@@ -9,29 +9,37 @@ experiment_name=$(echo $0 | sed -r 's/.*-(exp[0-9][0-9]-..*).sh/\1/')
 experiment_dir=$model_dir/$experiment_name
 mkdir -p $experiment_dir
 
-for operation in delete
-do
-    n_embed_dims=100
-    n_filters=1000
-    filter_width=5
-    n_fully_connected=2
-    n_hidden=100
+operation=delete
+n_embed_dims=56
+n_filters=10
+filter_width=6
+n_fully_connected=0
+n_hidden=0
 
-    echo ./train_keras.py $model_dir \
-        $data_dir/$operation-${errors}errors1word-distance-$distance.h5 \
-        $data_dir/$operation-${errors}errors1word-distance-$distance.h5 \
-        word \
-        --model-dest $experiment_dir/op_${operation}_n_embed_dims_${n_embed_dims}_n_filters_${n_filters}_filter_width_${filter_width}_n_fully_connected_${n_fully_connected}_n_hidden_${n_hidden} \
-        --target-name target \
-        --n-embeddings 61 \
-        --model-cfg n_embed_dims=$n_embed_dims n_filters=$n_filters filter_width=$filter_width n_fully_connected=${n_fully_connected} n_hidden=$n_hidden patience=10 \
-        --shuffle \
-        --confusion-matrix \
-        --classification-report \
-        --class-weight-auto \
-        --class-weight-exponent 3 \
-        --early-stopping-metric f2 \
-        --n-validation 100000 \
-        --log \
-        --verbose
-done | parallel --gnu -j 2
+for embedding_init in identity orthogonal uniform normal 
+do
+    for train_embeddings in false true
+    do
+        model_dest=$experiment_dir/op_${operation}_n_embed_dims_${n_embed_dims}_n_filters_${n_filters}_filter_width_${filter_width}_n_fully_connected_${n_fully_connected}_n_hidden_${n_hidden}_embedding_init_${embedding_init}_train_embeddings_${train_embeddings}
+            #--model-dest $model_dest \
+        echo $model_dest
+        ./train_keras.py $model_dir \
+            $data_dir/op-${operation}-distance-${distance}-errors-per-word-${errors}.h5 \
+            $data_dir/op-${operation}-distance-${distance}-errors-per-word-${errors}.h5 \
+            chars \
+            --target-name binary_target \
+            --n-embeddings 56 \
+            --model-cfg n_embed_dims=$n_embed_dims n_filters=$n_filters filter_width=$filter_width n_fully_connected=${n_fully_connected} n_hidden=$n_hidden embedding_init=$embedding_init train_embeddings=$train_embeddings optimizer=SGD learning_rate=0.001 momentum=0.0 decay=0.0 \
+            --shuffle \
+            --confusion-matrix \
+            --classification-report \
+            --class-weight-auto \
+            --class-weight-exponent 3 \
+            --verbose \
+            --n-train 50000 \
+            --n-epochs 3 \
+            --no-save
+            #--log \
+    done 
+done
+#| parallel --gnu -j 2
