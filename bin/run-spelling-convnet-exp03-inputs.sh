1d35248a40252475a01289331dc60ba9c0190808
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-02-21 11:37:35 -0700

    Final version of run-spelling-convnet-exp03-inputs.sh

--- bin/run-spelling-convnet-exp03-inputs.sh
+++ bin/run-spelling-convnet-exp03-inputs.sh
@@ -7,34 +7,42 @@ experiment_name=$(echo $0 | sed -r 's/.*-(exp[0-9][0-9]-..*).sh/\1/')
 experiment_dir=$model_dir/$experiment_name
 mkdir -p $experiment_dir
 
+n_embed_dims=10 
+n_filters=3000
+filter_width=6
+n_fully_connected=1
+n_residual_blocks=0
+n_hidden=1000
+
 for operation in delete insert substitute transpose
 do
-    for n_operations in 1 2 3
+    for n_operations in 1 2
     do
         for n_errors_per_word in 3 10
         do
-            n_embed_dims=100
-            n_filters=1000
-            filter_width=5
-            n_fully_connected=2
-            n_hidden=100
+            model_dest=$experiment_dir/op_${operation}_n_ops_${n_operations}_n_errors_per_word_${n_errors_per_word}
+            if [ -d $model_dest ]
+            then
+                continue
+            fi
             echo ./train_keras.py $model_dir \
-                $data_dir/$operation-${n_errors_per_word}n_errors_per_word1word-n_operations-$n_operations.h5 \
-                $data_dir/$operation-${n_errors_per_word}n_errors_per_word1word-n_operations-$n_operations.h5 \
-                word \
-                --model-dest $experiment_dir/op_${operation}_n_operations_${n_operations}_n_errors_per_word_per_word_${n_errors_per_word}_n_embed_dims_${n_embed_dims}_n_filters_${n_filters}_filter_width_${filter_width}_n_fully_connected_${n_fully_connected}_n_hidden_${n_hidden} \
-                --target-name target \
+                $data_dir/op-${operation}-distance-${n_operations}-errors-per-word-${n_errors_per_word}.h5 \
+                $data_dir/op-${operation}-distance-${n_operations}-errors-per-word-${n_errors_per_word}.h5 \
+                marked_chars \
+                --target-name binary_target \
+                --model-dest $model_dest \
                 --n-embeddings 61 \
-                --model-cfg n_embed_dims=$n_embed_dims n_filters=$n_filters filter_width=$filter_width n_fully_connected=${n_fully_connected} patience=10 \
+                --model-cfg n_embed_dims=$n_embed_dims n_filters=$n_filters filter_width=$filter_width n_fully_connected=$n_fully_connected n_residual_blocks=$n_residual_blocks n_hidden=$n_hidden patience=10 \
                 --shuffle \
                 --confusion-matrix \
                 --classification-report \
                 --class-weight-auto \
                 --class-weight-exponent 3 \
-                --early-stopping-metric f2 \
-                --n-validation 100000 \
-                --log \
-                --verbose
+                --early-stopping-metric val_f2 \
+                --checkpoint-metric val_f2 \
+                --verbose \
+                --log
+            break
         done
     done
 done | parallel --gnu -j 2
