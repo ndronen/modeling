a2fb5419fbd3689463ca98d798a685e2302f321c
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-02-23 20:13:38 -0700

    Snapshot of run-spelling-convnet-exp04-real-errors.sh

--- bin/run-spelling-convnet-exp04-real-errors.sh
+++ bin/run-spelling-convnet-exp04-real-errors.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 model_dir=models/keras/spelling/convnet
 data_dir=data/spelling/experimental/
@@ -17,22 +17,22 @@ n_hidden=1000
 # Train two models, one with random artificial errors, one with artificial
 # errors learned from a corpus of real errors.
 
-corpora=
+corpora="non-word-error-detection-experiment-04-random-negative-examples.h5 non-word-error-detection-experiment-04-generated-negative-examples.h5"
 
 for corpus in $corpora
 do
-    model_dest=$experiment_dir/op_${operation}_n_ops_${n_operations}_n_errors_per_word_${n_errors_per_word}
+    model_dest=$experiment_dir/$(echo $corpus | sed -e 's,-,_,g' -e 's,.h5,,')
     if [ -d $model_dest ]
     then
         continue
     fi
-    echo ./train_keras.py $model_dir \
+    ./train_keras.py $model_dir \
         $data_dir/$corpus \
-        $data_dir/exp04-real-errors.h5 \
+        $data_dir/$corpus \
         marked_chars \
         --target-name binary_target \
         --model-dest $model_dest \
-        --n-embeddings 61 \
+        --n-embeddings 255 \
         --model-cfg n_embed_dims=$n_embed_dims n_filters=$n_filters filter_width=$filter_width n_fully_connected=$n_fully_connected n_residual_blocks=$n_residual_blocks n_hidden=$n_hidden patience=10 \
         --shuffle \
         --confusion-matrix \
@@ -41,6 +41,8 @@ do
         --class-weight-exponent 3 \
         --early-stopping-metric val_f2 \
         --checkpoint-metric val_f2 \
+        --save-all-checkpoints \
         --verbose \
         --log
-done | parallel --gnu -j 2
+done 
+#| parallel --gnu -j 2
